ed853560b9a304db6605655719a6e5dbe4c96628
bmkopp10 <bmkopp10@gmail.com>
2021-05-07 11:40:01 -0500

    added axios, bootstrapvue and vue composition api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,10 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import {defineComponent} from "@vue/composition-api"; // @ is an alias to /src
 
-export default Vue.extend({
+export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld,
